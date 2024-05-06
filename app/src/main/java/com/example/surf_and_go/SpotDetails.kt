724c5a9215ede08c0574b1f136c4dd9210467536
npelcat <pelcat.nd@gmail.com>
2024-05-06 15:24:57 +0200

    add page spotDetail + display name and spot location

--- app/src/main/java/com/example/surf_and_go/SpotDetails.kt
+++ app/src/main/java/com/example/surf_and_go/SpotDetails.kt
@@ -1,6 +1,8 @@
 package com.example.surf_and_go
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,21 @@ class SpotDetails : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //Les variables "get" vont chercher la data de SpotsListActivity
+        val getName = intent.getStringExtra("spotName")
+
+        //Les variables "name, image, etc" sont des pointeurs
+        val name: TextView = findViewById<TextView>(R.id.name_detail)
+        name.text = getName
+
+        val getImage = intent.getStringExtra("spotImage")
+        val image: ImageView = findViewById<ImageView>(R.id.image_detail)
+      
+
+        val getLocation = intent.getStringExtra("spotLocation")
+        val location: TextView = findViewById<TextView>(R.id.location_detail)
+        location.text = getLocation
     }
 }
