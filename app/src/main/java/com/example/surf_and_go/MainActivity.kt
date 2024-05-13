8a84864eb40ab3499e7cc6b87370fd0b30633ce7
TimotheeRoy <timroy93100@gmail.com>
2024-05-13 15:39:18 +0200

    add wave sound on splash screen

--- app/src/main/java/com/example/surf_and_go/MainActivity.kt
+++ app/src/main/java/com/example/surf_and_go/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.surf_and_go
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,11 +12,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // démarre la musique de fond
-        //startService(Intent(this, BackgroundSoundService::class.java))
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -23,11 +24,29 @@ class MainActivity : AppCompatActivity() {
         // cache la bar d'action
         supportActionBar?.hide()
 
-
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, SpotsListActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1000)
+        }, 3900) //3900
+
+
+        // Initialiser le MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.waves)
+
+        // Définir un écouteur pour le MediaPlayer
+        mediaPlayer.setOnPreparedListener {
+            // La musique est prête à être jouée
+            mediaPlayer.start()
+        }
+
+
+
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        // Libérer les ressources du MediaPlayer
+        mediaPlayer.release()
+    }
+
 }
